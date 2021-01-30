4597fefc1134188a9a1f248d847695376376ae90
Magali Fabri <magali@Magalis-MacBook-Pro.local>
2021-01-30 18:04:39 +0100

    update

--- setup.sh
+++ setup.sh
@@ -9,11 +9,13 @@ eval $(minikube -p minikube docker-env)
 docker build ./srcs/nginx -t nginx_img
 # docker build ./srcs/mysql -t mysql_img
 # docker build ./srcs/phpmyadmin -t pma_img
+# docker build ./srcs/wordpress -t wp_img
 
 # create a deployment and a service in minikube from the nginx image
-kubectl create -f ./srcs/nginx/nginx.yaml
+kubectl apply -f ./srcs/nginx/nginx.yaml
 # kubectl apply -f ./srcs/mysql/mysql.yaml 
 # kubectl apply -f ./srcs/phpmyadmin/pma.yaml 
+# kubectl apply -f ./srcs/wordpress/wp.yaml 
 
 # have minikube expose the service locally
 minikube service nginx-service
