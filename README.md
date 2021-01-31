be8c3d0538db311e2e2cd026023afd05d1ac0fdf
Magali Fabri <magali@Magalis-MacBook-Pro.local>
2021-01-31 16:11:03 +0100

    update

---
+++ README.md
@@ -0,0 +1,16 @@
+# ft_services - WORK IN PROGRESS
+_Project of coding school 19 in Brussels (part of the 42 school network)_
+
+## Resources
+- [Kubernetes Tutorial for Beginners [FULL COURSE in 4 Hours]](https://www.youtube.com/watch?v=X48VuDVv0do)
+
+- [How to Run Locally Built Docker Images in Kubernetes](https://medium.com/swlh/how-to-run-locally-built-docker-images-in-kubernetes-b28fbc32cc1d)
+- [Accessing apps: How to access applications running within minikube # Using minikube tunnel](https://minikube.sigs.k8s.io/docs/handbook/accessing/#using-minikube-tunnel)
+
+- MetalLB
+	- [MetalLB (Network LoadBalancer ) & Minikube.](https://medium.com/@shoaib_masood/metallb-network-loadbalancer-minikube-335d846dfdbe)
+	- [MetalLB Configuration in Minikube — To enable Kubernetes service of type “LoadBalancer”](https://medium.com/faun/metallb-configuration-in-minikube-to-enable-kubernetes-service-of-type-loadbalancer-9559739787df)
+	- [metallb.universe.tf: Installation](https://metallb.universe.tf/installation/)
+	- [metallb.universe.tf: Configuration](https://metallb.universe.tf/configuration/)
+
+- [installing MySQL on Alpine](https://wiki.alpinelinux.org/wiki/Mysql)
--- setup.sh
+++ setup.sh
@@ -5,24 +5,24 @@ minikube start --vm-driver=hyperkit
 # cause minikube to look for images locally
 eval $(minikube -p minikube docker-env)
 
-# build an image that runs nginx
+# build the container images with Docker
 docker build ./srcs/nginx -t nginx_img
+docker build ./srcs/wordpress -t wp_img
 # docker build ./srcs/mysql -t mysql_img
 # docker build ./srcs/phpmyadmin -t pma_img
-# docker build ./srcs/wordpress -t wp_img
 
 # set up metalLB
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 kubectl apply -f configmap.yaml
 
-# create a deployment and a service in minikube from the nginx image
+# create the deployments and services from the .yaml files
 kubectl apply -f ./srcs/nginx/nginx.yaml
+kubectl apply -f ./srcs/wordpress/wp.yaml
 # kubectl apply -f ./srcs/mysql/mysql.yaml 
 # kubectl apply -f ./srcs/phpmyadmin/pma.yaml 
-# kubectl apply -f ./srcs/wordpress/wp.yaml 
 
+# "The memberlist secret contains the secretkey to encrypt the communication between speakers for the fast dead node detection."" - https://metallb.universe.tf/installation/
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
-# have minikube expose the service locally
-minikube service nginx-service
+# view a specific service in the browser with the `minikube service <service name>` command or run `minikube tunnel` in a seperate shell window to access the external IPs given by MetalLB from the browser.
