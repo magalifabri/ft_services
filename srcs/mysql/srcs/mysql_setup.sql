4077d62370df5cc47bda05ba4d1557726e594d39
Magali Fabri <magali@Magalis-MacBook-Pro.local>
2021-02-08 12:54:48 +0100

    auto wp site (+ users) gen

--- srcs/mysql/srcs/mysql_setup.sql
+++ srcs/mysql/srcs/mysql_setup.sql
@@ -13,4 +13,7 @@ GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'user'@'%' IDENTIFIED BY 'pass';
 -- run script that will create the phpmyadmin database and populate it with the required tables
 \. create_tables.sql
 
+-- run script that will put all the tables in place to create the WordPress site
+\. wordpress_db.sql
+
 exit
