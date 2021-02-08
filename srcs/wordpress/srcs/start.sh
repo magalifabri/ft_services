575bdcf831e77b9574c67de6b3e80e691dea45f1
Magali Fabri <magali@Magalis-MacBook-Pro.local>
2021-02-08 19:02:13 +0100

    nginx instead of php -s

--- srcs/wordpress/srcs/start.sh
+++ srcs/wordpress/srcs/start.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
-# start Telegraf in the background with the appropriate configurations
+# start Telegraf with the appropriate configurations (in the background)
 telegraf --config telegraf.conf &
 
+# start php-fpm7 for processing php requests (in the background)
+php-fpm7 &
+
+# start NGINX (`deamon off` keeps the process running)
+nginx -g "daemon off;"
+
+
 # start PHP web server with the document root specified by -t
-/usr/bin/php -S 0.0.0.0:5050 -t /var/www/html/wp/
+# /usr/bin/php -S 0.0.0.0:5050 -t /var/www/html/wp/
