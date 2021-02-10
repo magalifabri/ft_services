a2f1d9cd304d4695eb3be34ad6619580d9212e60
Magali Fabri <magali@Magalis-MacBook-Pro.local>
2021-02-10 16:20:51 +0100

    disable SSL

--- srcs/nginx/srcs/start.sh
+++ srcs/nginx/srcs/start.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 # make some keys for the SSH daemon to play with (otherwise it won't start) and start it
-ssh-keygen -A
-/usr/sbin/sshd
+# ssh-keygen -A
+# /usr/sbin/sshd
 
 # create a user to connect to with SSH
-adduser -D magali
-echo magali:pass | chpasswd
+# adduser -D magali
+# echo magali:pass | chpasswd
 
 # start Telegraf in the background with the appropriate configurations
 telegraf --config telegraf.conf &
